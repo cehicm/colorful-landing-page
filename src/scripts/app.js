e8175892dabf51d56394e2bca7e535dc22fb09ec
Marija Cehic <marijac@stuntcoders.com>
2022-03-03 17:32:20 +0100

    Remove header title height, fix menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,51 +1,42 @@
 //Navigation open/close
-(function () {
-	function disableScroll() {
-		document.body.addEventListener("touchmove", preventDefault, {
-			passive: false,
-		});
-	}
+(function() {
+    const menuBtn = $(".menu__btn");
+    const btnClose = $(".menu__btn--close");
+    const menu = $(".nav__menu");
 
-	function enableScroll() {
-		document.body.removeEventListener("touchmove", preventDefault, {
-			passive: false,
-		});
-	}
-	const menuBtn = $(".menu__btn");
-	const btnClose = $(".menu__btn--close");
-	const menu = $(".nav__menu");
+    $(menuBtn).on("click", function() {
+        menu.toggleClass("show-menu");
+        menuBtn.addClass("display-none");
+        btnClose.addClass("display-block");
 
-	$(menuBtn).on("click", function () {
-		menu.toggleClass("show-menu");
-		menuBtn.addClass("display-none");
-		btnClose.addClass("display-block");
+        //Prevent user from scrolling when menu is open
+        $("body").addClass("body-menu-open");
+    });
 
-		disableScroll();
-	});
+    $(btnClose).on("click", function() {
+        menu.toggleClass("show-menu");
+        menuBtn.removeClass("display-none");
+        btnClose.removeClass("display-block");
 
-	$(btnClose).on("click", function () {
-		menu.toggleClass("show-menu");
-		menuBtn.removeClass("display-none");
-		btnClose.removeClass("display-block");
-
-		enableScroll();
-	});
+        //Allow user to scroll when menu is closed again
+        $("body").addClass("body-menu-open");
+    });
 })(jQuery);
 
 //Gallery open/close
-(function () {
-	$(".btn--see-all").on("click", function () {
-		$(this).nextAll().slideToggle("slow");
+(function() {
+    $(".btn--see-all").on("click", function() {
+        $(this).nextAll().slideToggle("slow");
 
-		if ($(this).text() === "SEE ALL") {
-			$(this).text("SEE LESS");
-		} else if ($(this).text() === "SEE LESS") {
-			$(this).text("SEE ALL");
-		}
-	});
+        if ($(this).text() === "SEE ALL") {
+            $(this).text("SEE LESS");
+        } else if ($(this).text() === "SEE LESS") {
+            $(this).text("SEE ALL");
+        }
+    });
 })(jQuery);
 
 // Disable/enable scroll functions for mobile menu
 function preventDefault(e) {
-	e.preventDefault();
+    e.preventDefault();
 }
